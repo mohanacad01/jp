2e923ba466636ed41df44786f6394645c57672bc
mohanacad01 <mohanacad01@brightskills.com>
2024-01-08 17:56:07 +0530

    files added to js

---
+++ js/javascript.js
@@ -0,0 +1,102 @@
+//alert("welcome to js");
+//function  Demo()
+{
+var a=20;
+var b=30;
+var c;
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("addition")
+c=a+b;
+console.log("added value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("subraction")
+c=a-b;
+console.log("subtract value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("multiplication")
+c=a*b;
+console.log("multiplied value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("division")
+c=a/b;
+console.log("divided value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("modulation")
+c=a%b;
+console.log("moduled value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("assignment")
+c=a=b;
+console.log("assigned value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("logical")
+c=a>b;
+console.log("logical value of a and b is..."+c);
+
+console.log (a==b);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("comparision")
+c=a<b;
+console.log("compared value of a and b is..."+c);
+
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("coparision")
+c=a>=b;
+console.log("compared value of a and b is..."+c);
+
+
+console.log (a<=b);
+
+console.log ("value of A is..."+a);
+console.log ("value of B is..."+b)
+console.log("comparition")
+c=a!=b;
+console.log("compared value of a and b is..."+c);
+
+console.log ("value of A is..."+a);
+console.log("post increment")
+a++;
+c=a;
+console.log("increment value of a is..."+c);
+
+
+console.log (a>b)&&(a==b);
+console.log (++a);
+console.log (++b);
+console.log (a**=b);
+console.log (a>b)||(a<b);
+console.log ((a<b)?"true":"false");
+
+
+}
+Demo();
+
+
+\\ const car={
+    carrtype:"petrolk", model:"2020", brand:"maruthi suzuki",enginetype:"hybrid"
+
+}
+const (car.type);
+const (car.model);
+const (car.Etype);
+
+
+
+
